a993e3f342bb3c485ccdc65c305078c9f4171b79
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2025-07-15 22:49:26 +0100

    v1.1.1

--- src/i18n.js
+++ src/i18n.js
@@ -15,7 +15,7 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: "/src/locales/{{lng}}/translation.json",
+      loadPath: "/locales/{{lng}}/translation.json",
     },
     react: {
       useSuspense: false,
